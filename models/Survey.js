cc6ea77f7e2ac60bc43f6f8137994cc569e28892
Francisco Paz <francisco.paz@telusinternational.com>
2019-07-01 11:28:14 -0600

    employee model

--- models/Survey.js
+++ models/Survey.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const RecipientSchema = require('./Recipient');
 
-const surveySchema = new Schema({
-    title: String,
-    body: String,
-    subject: String,
-    recipients: [RecipientSchema],
-    yes: { type: Number, default: 0 },
-    no: { type: Number, default:0 },
-    _user: { type: Schema.Types.ObjectId, ref: 'User' },
-    dateSent: Date,
-    lastResponded: Date
+const employeeSchema = new Schema({
+    employeeID: String,
+    department: String,
+    costCenter: String,
+    position: String,
+    site: String,
+    country: String,
+    supervisorID: [employeeSchema],
+    firstName: String,
+    secondName: String,
+    lastName: String,
+    email: String,
+    isSupervisor: { type: Number, default: 0 },
+    status: { type: Number, default:1 },
+    costCenterID: { type: String, default:"CC-999-VOXP" }
 });
 
-mongoose.model('surveys', surveySchema);
+mongoose.model('surveys', employeeSchema);
