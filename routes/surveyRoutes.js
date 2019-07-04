90dad47a7564e4834db557c73f8b5399afa373e0
Francisco Paz <francisco.paz@telusinternational.com>
2019-07-04 10:44:34 -0600

    retrieve all employees

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,9 +11,8 @@ const Employee = mongoose.model('employees');
 
 module.exports = app => {
     app.get('/api/surveys', requireLogin, async (req, res) => {
-        const surveys = await Employee.find({ _user: req.user.id });
-
-        res.send(surveys);
+        const employees = await Employee.find();
+        res.send(employees);
     });
 
     app.get('/api/surveys/:surveyId/:choice', (req, res) => {
