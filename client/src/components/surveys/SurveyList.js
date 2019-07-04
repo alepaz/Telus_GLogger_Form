f56dd11fa7fd466269f25a2e3a8d6c843e6bcb98
Francisco Paz <francisco.paz@telusinternational.com>
2019-07-04 13:45:56 -0600

    retrieve employees list

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -12,17 +12,27 @@ class SurveyList extends Component {
             return(
                 <div className="card darken-1" key={survey._id}>
                     <div className="card-content text-white">
-                        <span className="card-title">{survey.title}</span>
-                        <p>
-                            {survey.body}
-                        </p>
-                        <p className="right">
-                            Sent On: {new Date(survey.dateSent).toLocaleDateString()}
-                        </p>
-                    </div>
-                    <div className="card-action">
-                        <a>Yes: {survey.yes}</a>
-                        <a>No: {survey.no}</a>
+                        <span className="card-title">{survey.firstName} {survey.secondName} {survey.lastName}</span>
+                        <div class="card-content">
+                            <p> <b style={{fontWeight: "bold"}}>Site: </b>
+                                {survey.site}
+                            </p>
+                            <p> <b style={{fontWeight: "bold"}}>Supervisor: </b>
+                                {survey.supervisorID}
+                            </p>                            
+                            <p> <b style={{fontWeight: "bold"}}>Temporal ID: </b>
+                                {survey.employeeID}
+                            </p>
+                            <p> <b style={{fontWeight: "bold"}}>Position: </b>
+                                {survey.position}
+                            </p>
+                            <p> <b style={{fontWeight: "bold"}}>Department: </b>
+                                {survey.department}
+                            </p>
+                            <p> <b style={{fontWeight: "bold"}}>Email: </b>
+                                {survey.email}
+                            </p>                            
+                        </div>
                     </div>
                 </div>
             );
@@ -35,9 +45,7 @@ class SurveyList extends Component {
                 {this.renderSurveys()}
             </div>
         );
-
     }
-
 }
 
 function mapStateToProps({ surveys }) {
