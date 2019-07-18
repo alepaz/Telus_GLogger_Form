aa2289f7582b3b3cc86a4ae6aefcc52c36ecc142
Francisco Paz <francisco.paz@telusinternational.com>
2019-07-18 17:58:36 -0600

    setup proxy

--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -1,14 +1,13 @@
 const proxy = require('http-proxy-middleware')
  
 module.exports = function(app) {
-    app.use(proxy('/auth/google/callback', { target: 'http://webappstest02.telusinternational.com:5000' }))
-    app.use(proxy('/auth/google', { target: 'http://webappstest02.telusinternational.com:5000' }))
-    app.use(proxy('/api/employees', { target: 'http://webappstest02.telusinternational.com:5000' }))
-    app.use(proxy('/api/employees/:id', { target: 'http://webappstest02.telusinternational.com:5000' }))
-    app.use(proxy('/api/top_employees/', { target: 'http://webappstest02.telusinternational.com:5000' }))
-    app.use(proxy('/api/csv_employees/', { target: 'http://webappstest02.telusinternational.com:5000' }))
-    app.use(proxy('/api/count_employees/', { target: 'http://webappstest02.telusinternational.com:5000' }))
-    app.use(proxy('/api/employee', { target: 'http://webappstest02.telusinternational.com:5000' }))
-    app.use(proxy('/api/*', { target: 'http://webappstest02.telusinternational.com:5000' }))
-    //app.use(proxy('/api/*', { target: 'http://172.25.29.30:5000' }))
+    app.use(proxy('/auth/google/callback', { target: 'http://localhost:5000' }))
+    app.use(proxy('/auth/google', { target: 'http://localhost:5000' }))
+    app.use(proxy('/api/employees', { target: 'http://localhost:5000' }))
+    app.use(proxy('/api/employees/:id', { target: 'http://localhost:5000' }))
+    app.use(proxy('/api/top_employees/', { target: 'http://localhost:5000' }))
+    app.use(proxy('/api/csv_employees/', { target: 'http://localhost:5000' }))
+    app.use(proxy('/api/count_employees/', { target: 'http://localhost:5000' }))
+    app.use(proxy('/api/employee', { target: 'http://localhost:5000' }))
+    app.use(proxy('/api/*', { target: 'http://localhost:5000' }))
 }
