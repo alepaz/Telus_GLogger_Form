6c2ea489c962722b4b123fd9bb52d8db0ecaa0b5
Jonathan Palma <tanpalma04@gmail.com>
2019-07-18 00:14:48 -0600

    Created async selectable

---
+++ ui/src/services/employeeService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import stringifyRequestParams from "helpers/stringifyRequestParams";
+
+const getEmployees = async (requestParams = {}) => {
+  const endpoint = "/api/employees";
+  const expectedParams = ["limit", "filter"];
+  const params = stringifyRequestParams(requestParams, expectedParams);
+  const url = `${endpoint}${params}`;
+  const response = await axios.get(url);
+  return response;
+};
+
+export default {
+  getEmployees
+};
