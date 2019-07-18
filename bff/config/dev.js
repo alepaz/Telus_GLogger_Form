ea8e98bd9687957e57b6c8f4a1736087a777a952
Francisco Paz <francisco.paz@telusinternational.com>
2019-07-18 15:32:49 -0600

    configurations for setUpProxy

--- bff/config/dev.js
+++ bff/config/dev.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+    googleClientID: '193585639610-mrvoo6dbq3i00gm9jmlhf3nship9h89s.apps.googleusercontent.com',
+    googleClientSecret: 's45Ej8_jE2vcueU1YXxNp6RG',
+    //mongoURI: 'mongodb://alepaz:z20020052@ds119304.mlab.com:19304/emaily',
+    mongoURI: 'mongodb://is:isdev@172.25.29.30:27017/glform',
+    //mongoURI: 'mongodb://localhost:27017/admin',
+    cookieKey: 'asdjansjkfndfkjsdmfmwodrwpeodaskdfkmdsfwerofmwoewenfuwenfuwefnuwefnweufn',
+    stripePublishableKey: 'pk_test_JPhv7J2ZlzEruXMO8DimNc3a',
+    stripeSecretKey: 'sk_test_YBpQi5FbH9CYNXAw0n7CmcH1',
+    sendGridKey: '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',
+    redirectDomain: 'http://localhost:3000'
+};
