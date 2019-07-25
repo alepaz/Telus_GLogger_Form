9f6b0d12f222f28520349da91144ffaab158a313
Francisco Paz <francisco.paz@telusinternational.com>
2019-07-24 18:53:29 -0600

    Lion Image and update landing

--- ui/src/components/Landing.js
+++ ui/src/components/Landing.js
@@ -4,11 +4,12 @@ import Box from '@tds/core-box';
 import Button from '@tds/core-button';
 import DisplayHeading from '@tds/core-display-heading';
 import Paragraph from '@tds/core-paragraph';
+import lionImage from '../images/Lion_Looking_at_Laptop.jpg'
 
 const Landing = () => {
     return (
     <Box between={5}>
-        <div><br/></div>
+        <img className="responsive-img" src={lionImage} alt="lion" style={{maxWidth:450, margin:"auto", paddingTop:5}} />
         <DisplayHeading>Register users in GLogger for a temporal instance through this application.</DisplayHeading>
 
         <div>
